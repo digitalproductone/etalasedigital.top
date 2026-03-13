23013409e7feb316680cce0910b263ae7a1af55f
Digital Product One <digitalproductoneclub@gmail.com>
2026-03-13 11:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzJGxy9tgA4cBTepnt045h10TVdcGNDyfMfXK0weMsMi6o3wX2aOO2DtNllkI7yy4nBdA/exec";
+window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzQTBcjRIJG69s1kO2F2IM2Ep9nwSbTPFnowOtUkkCXMijjmivIevQBExJSTYf3EmH19Q/exec";
